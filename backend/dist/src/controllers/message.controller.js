e7d3b8f043a27f023dca2ba94170be432011797d
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2025-04-07 17:50:08 +0530

    configuration for the deployment

---
+++ backend/dist/src/controllers/message.controller.js
@@ -0,0 +1,110 @@
+import prisma from "../db/prisma.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+export const sendMessage = async (req, res) => {
+    try {
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user.id; // coming from middleware
+        // check if previous conversation exist
+        let conversation = await prisma.conversation.findFirst({
+            where: {
+                participantIds: {
+                    hasEvery: [senderId, receiverId],
+                },
+            },
+        });
+        // create new conversation
+        if (!conversation) {
+            conversation = await prisma.conversation.create({
+                data: {
+                    participantIds: {
+                        set: [senderId, receiverId],
+                    },
+                },
+            });
+        }
+        // create new message
+        const newMessage = await prisma.message.create({
+            data: {
+                senderId,
+                body: message,
+                conversationId: conversation.id,
+            },
+        });
+        // update/add message to conversation
+        if (newMessage) {
+            conversation = await prisma.conversation.update({
+                where: {
+                    id: conversation.id,
+                },
+                data: {
+                    messages: {
+                        connect: {
+                            id: newMessage.id,
+                        },
+                    },
+                },
+            });
+        }
+        // socket io
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+        res.status(201).json(newMessage);
+    }
+    catch (error) {
+        console.log("error in sendMessage", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const senderId = req.user.id;
+        const conversation = await prisma.conversation.findFirst({
+            where: {
+                participantIds: {
+                    hasEvery: [senderId, userToChatId],
+                },
+            },
+            include: {
+                messages: {
+                    orderBy: {
+                        createdAt: "asc",
+                    },
+                },
+            },
+        });
+        if (!conversation) {
+            return res.status(200).json([]);
+        }
+        res.status(200).json(conversation.messages);
+    }
+    catch (error) {
+        console.log("Error in getMessages: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const authUser = req.user.id;
+        const users = await prisma.user.findMany({
+            where: {
+                id: {
+                    not: authUser,
+                },
+            },
+            select: {
+                id: true,
+                fullName: true,
+                profilePic: true,
+            },
+        });
+        res.status(200).json(users);
+    }
+    catch (error) {
+        console.log("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -1,37 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
 import dotenv from "dotenv";
 import { app, server } from "./socket/socket.js";
-
 dotenv.config();
-
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
-
 // const app = express();
-
 app.use(cookieParser()); // to parse the cookies
 app.use(express.json()); // for parsing application/json data
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV !== "development") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
-}
-
 app.get("/", (req, res) => {
-  res.send("Hello world 2");
+    res.send("Hello world 2");
 });
-
 server.listen(PORT, () => {
-  console.log("server running on port 3000");
+    console.log("server running on port 3000");
 });
